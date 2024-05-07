13b601a97520a44ea499239582c190cbd3db1805
Carlo-Adile <adile.carlo.a@gmailcom>
2024-05-07 17:40:48 +0200

    commit current progress on card cycle

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="container">
-    <h2>Home Page</h2>
+  <div class="container-fluid">
+    {{-- jumbotron --}}
+    <div id="jumbotron">
+      <img src="{{ Vite::asset('resources/img/jumbotron.jpg')}}" alt="">
+    </div>
+    {{-- comics --}}
+    <div class="row g-4">
+      <h2>CARD HERE:</h2>
+      @foreach ($comics as $key => $comic)
+        <div class="col">
+        
+          <div class="card border-0" id="comic_thumb">
+            <img src="{{ $comics['thumb']}}" alt="" class="card-img-top img-fluid">
+            <div class="card-body">
+              {{$comics['title']}}
+            </div>
+          </div>
+
+        </div>
+      @endforeach
+      
+    </div>
   </div>
+  
 
 @endsection
 
