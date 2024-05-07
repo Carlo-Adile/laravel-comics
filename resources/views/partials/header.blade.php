af8278d5f0afc5282b6c1810c3faf6b3c33dad46
Carlo-Adile <adile.carlo.a@gmailcom>
2024-05-07 16:02:39 +0200

    commit current progress

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,24 @@
-<h1>
-  Header
-</h1>
+<header>
+  <nav class="navbar navbar-expand-lg justify-content-center">
+    <div class="container">
+      <a href="" class="navbar-brand">
+        <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="" width="30" height="24">
+      </a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a href="" class="nav-link">CHARACTERS</a></li>
+          <li class="nav-item"><a href="" class="nav-link">COMICS</a></li>
+          <li class="nav-item"><a href="" class="nav-link">MOVIES</a></li>
+          <li class="nav-item"><a href="" class="nav-link">TV</a></li>
+          <li class="nav-item"><a href="" class="nav-link">GAMES</a></li>
+          <li class="nav-item"><a href="" class="nav-link">COLLECTIBLES</a></li>
+          <li class="nav-item"><a href="" class="nav-link">VIDEOS</a></li>
+          <li class="nav-item"><a href="" class="nav-link">FANS</a></li>
+          <li class="nav-item"><a href="" class="nav-link">NEWS</a></li>
+          <li class="nav-item"><a href="" class="nav-link">SHOP</a></li>
+        </ul>
+      </div>
+      
+    </div>
+  </nav>
+</header>
